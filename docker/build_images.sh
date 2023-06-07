0318fab0b1c90ba29be988c0f769b06d1b6d5da9
cdickson <cdickson@uncharted.software>
2023-06-07 11:37:52 -0400

    rename images

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -8,7 +8,7 @@ cp spring-cloud-skipper/spring-cloud-skipper-server/target/spring-cloud-skipper-
 popd
 
 # Build dataflow-server
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/pantera-dataflow-server:"$VERSION" -f Dockerfile-dataflow .
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/spring-cloud-dataflow-server:"$VERSION" -f Dockerfile-dataflow .
 
 # Build skipper-server
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/pantera-skipper-server:"$VERSION" -f Dockerfile-skipper .
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/spring-cloud-skipper-server:"$VERSION" -f Dockerfile-skipper .
--- docker/push_images.sh
+++ docker/push_images.sh
@@ -5,7 +5,7 @@ VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
 popd
 
 # Build dataflow-server
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/pantera-dataflow-server:"$VERSION" -f Dockerfile-dataflow . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/spring-cloud-dataflow-server:"$VERSION" -f Dockerfile-dataflow . --push
 
 # Build skipper-server
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/pantera-skipper-server:"$VERSION" -f Dockerfile-skipper . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/unchartedsoftware/spring-cloud-skipper-server:"$VERSION" -f Dockerfile-skipper . --push
